5689659e34942910a335968f002d68a9a23573cc
Adonis Ling <adonis0147@gmail.com>
2026-05-23 00:14:19 +0800

    chore: bump the version of HBase

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -15,35 +15,35 @@ fi
 
 if [[ "${arch}" == 'x86_64' ]]; then
 	# shellcheck disable=2034
-	declare -r JAVA_URL="https://download.oracle.com/java/17/archive/jdk-17.0.12_linux-${arch/86_/}_bin.tar.gz"
+	declare -r JAVA_URL="https://corretto.aws/downloads/resources/17.0.19.10.1/amazon-corretto-17.0.19.10.1-linux-${arch/86_/}.tar.gz"
 	# shellcheck disable=2034
-	declare -r JAVA_SHA256SUM='311f1448312ecab391fe2a1b2ac140d6e1c7aea6fbf08416b466a58874f2b40f'
+	declare -r JAVA_SHA512SUM='c2ec54f90191e99dc551e3c072d3ea9e4e20b938162bfbc79cd90ea1065e41705950998f89c134842c8957a8eab29a2a1539512f130d2e8f6370573af564bb7c'
 else
 	# shellcheck disable=2034
-	declare -r JAVA_URL="https://download.oracle.com/java/17/archive/jdk-17.0.12_linux-${arch}_bin.tar.gz"
+	declare -r JAVA_URL="https://corretto.aws/downloads/resources/17.0.19.10.1/amazon-corretto-17.0.19.10.1-linux-${arch}.tar.gz"
 	# shellcheck disable=2034
-	declare -r JAVA_SHA256SUM='31c2fa06f3f98d92984a86269c71e6b9e956272084f3a1d2db6d07e6164b2f4f'
+	declare -r JAVA_SHA512SUM='b90dafc4aa4f1d4b26f7081330116b811e2ef3203cdaaf932ee8f7190a886e34cb49309395eceac1efc365312be3e113daa6eafc0e6dcfa594469608c1e7e279'
 fi
 
 # shellcheck disable=2034
-declare -r HBASE_URL='https://archive.apache.org/dist/hbase/2.6.2/hbase-2.6.2-bin.tar.gz'
+declare -r HBASE_URL='https://archive.apache.org/dist/hbase/2.6.5/hbase-2.6.5-bin.tar.gz'
 # shellcheck disable=2034
-declare -r HBASE_SHA256SUM='5ff9a3993031f5a9e1d94fd795fb9ff8ee003b90570e476c63af6d3dd744a583'
+declare -r HBASE_SHA512SUM='67c1f59b7530a6f02cce6d3df8c0ba620130862a31828331584eee892922c37fbdb32b9e5320e897e1557d352e2f54aa0ddc097780abcd841a8d494134320a1a'
 
 function download() {
 	local package
 	package="$(echo "${1}" | awk '{print toupper($0)}')"
 	local url_variable="${package}_URL"
-	local checksum_variable="${package}_SHA256SUM"
+	local sha512sum_variable="${package}_SHA512SUM"
 
 	local url="${!url_variable}"
-	local checksum="${!checksum_variable}"
+	local checksum="${!sha512sum_variable}"
+	local checksum_command
+	checksum_command="$(command -v gsha512sum || command -v sha512sum)"
 	local filename
 	filename="$(basename "${url}")"
 
-	local SHA256SUM
-	SHA256SUM="$(command -v gsha256sum || true)"
-	if [[ ! -f "${filename}" ]] || ! echo "${checksum} ${filename}" | "${SHA256SUM:-sha256sum}" --check &>/dev/null; then
+	if [[ ! -f "${filename}" ]] || ! echo "${checksum} ${filename}" | "${checksum_command}" --check &>/dev/null; then
 		curl -LO "${url}"
 	fi
 }
